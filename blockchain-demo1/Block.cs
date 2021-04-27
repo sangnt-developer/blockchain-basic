dcd039face6667f598b442bcd79088120fb534d6
Sang Nguyen <sangnt.developer@gmail.com>
2021-04-27 21:42:14 +0700

    Add project files.

---
+++ blockchain-demo1/Block.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blockchain_demo1
+{
+    public class Block
+    {
+        public int Index { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string PreviousHash { get; set; }
+        public string Hash { get; set; }
+        public string Data { get; set; }
+
+        public Block(DateTime timeStamp, string previousHash, string data)
+        {
+            Index = 0;
+            TimeStamp = timeStamp;
+            PreviousHash = previousHash;
+            Data = data;
+            Hash = CalculateHash();
+
+        }
+
+        public string CalculateHash()
+        {
+            var unhashedString = $"{TimeStamp}-{PreviousHash ?? ""}-{Data}";
+            byte[] inputBytes = Encoding.ASCII.GetBytes(unhashedString);
+
+            SHA256 sha256 = SHA256.Create();
+            byte[] outputBytes = sha256.ComputeHash(inputBytes);
+
+            return Convert.ToBase64String(outputBytes);
+        }
+    }
+}
